038314b40f2f4134fa024fcaae39287e53a8809d
BakuEmilov <emilovbaku530@gmail.com>
2024-02-21 22:27:09 +0600

    We have finished the pricing section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Faq from "./components/Faq";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
 
 export default function Home() {
   return ( 
@@ -11,6 +12,7 @@ export default function Home() {
       <div className="px-[20px] lg:px-20 mx-auto">
         <Features/>
         <Faq/>
+        <Pricing/>
       </div>
     </div>
   );
