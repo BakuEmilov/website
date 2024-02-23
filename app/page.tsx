b5fffb97ff7d8ac25fed2cf02efbc5b4473744d2
BakuEmilov <emilovbaku530@gmail.com>
2024-02-23 14:42:51 +0600

    have you finished the footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
+import Cta from "./components/Cta";
 import Faq from "./components/Faq";
 import Features from "./components/Features";
+import { Footer } from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
 
 export default function Home() {
-  return ( 
+  return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <div className="px-[20px] lg:px-20 mx-auto">
-        <Features/>
-        <Faq/>
-        <Pricing/>
+        <Features />
+        <Faq />
+        <Pricing />
+        <Cta />
+        <Footer />
       </div>
     </div>
   );
